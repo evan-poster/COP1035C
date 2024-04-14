89c142c2125e9ff093d20364d3445c0e2d21250e
evan-poster <evan.poster@gmail.com>
2024-04-14 13:51:46 -0400

    Ch3/Ex1 added

---
+++ Ch3 Exercises/program1.py
@@ -0,0 +1,10 @@
+# Get lengths of three sides of a triangle from the user
+side_a = float(input("Enter the length of side A: "))
+side_b = float(input("Enter the length of side B: "))
+side_c = float(input("Enter the length of side C: "))
+
+# Check if the triangle is equilateral
+if side_a == side_b and side_b == side_c:
+    print("The triangle is equilateral.")
+else:
+    print("The triangle is not equilateral.")
