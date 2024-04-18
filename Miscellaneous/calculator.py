fd474339b77a41369671f35c12dddf35a07c80fc
evan-poster <evan.poster@gmail.com>
2024-04-18 10:18:27 -0400

    calculator.py added calculate_return()

--- Miscellaneous/calculator.py
+++ Miscellaneous/calculator.py
@@ -1,5 +1,8 @@
-
+# Personal function for calculating return
+# Based on: principal, percent of principal, roi, iterations
+def calculate_return(principal, percent, roi, iterations):
+    return (principal * roi)  * percent ** iterations
 
 
 if __name__ == "__main__":
-    print("Hello World")
+    pass
