9f68c9b08717c1d49bb732cec879e1b9f246fa08
evan-poster <evan.poster@gmail.com>
2024-04-18 10:18:05 -0400

    calendar.py updated; leap_year_rules added

--- Miscellaneous/calendar.py
+++ Miscellaneous/calendar.py
@@ -1,4 +1,17 @@
 
+test_sets = [
+    ("JAN 2001", 31),
+    ("FEB 2001", 28),
+    ("MAR 2001", 31),
+    ("SEP 1752", 19),
+    ("FEB 1900", 28),
+    ("FEB 2000", 29),
+    ("FEB 2004", 29),
+    ("FEB 1700", 28),
+    ("FEB 1800", 28),
+    ("FEB 2100", 28),
+]
+
 my_table = {
     "JAN": 31,
     "FEB": 28,
@@ -17,15 +30,34 @@ my_table = {
 
 # Report the number of days in the month
 def days_in_month(month_year):
-    # Account for leap year
+    # Account for leap years and their exceptions
     if month_year[0] == "FEB":
         if int(month_year[1]) % 4 == 0:
-            if int(month_year[1]) % 400 == 0 or int(month_year[1]) % 100 != 0:
-                return my_table.get(month_year[0]) + 1
-            return my_table.get(month_year[0])
-    return my_table.get(month_year[0])
+            if int(month_year[1]) % 100 == 0:
+                if int(month_year[1]) % 400 == 0:
+                    return 29
+                else:
+                    return 28
+            else:
+                return 29
+        else:
+            return 28
+    if month_year[0] == "SEP" and month_year[1] == "1752":
+        return 19
+    else:
+        return my_table[month_year[0]]
 
 
 if __name__ == "__main__":
-    data = input("Enter a month and year with format: FEB 2000: ").upper().strip().split()
-    print(days_in_month(data))
+    # data = input("Enter a month and year with format: FEB 2000: ").upper().strip().split()
+    # print(days_in_month(data))
+    pass_count = 0
+    for test in test_sets:
+        if days_in_month(test[0].split()) == test[1]:
+            pass_count += 1
+        else:
+            print(f"FAIL ({test[0]})")
+    if pass_count == len(test_sets):
+        print("Passed all tests")
+    else:
+        print(f"Passed {pass_count} out of {len(test_sets)} tests")
