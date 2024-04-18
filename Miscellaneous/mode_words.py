10a427204542388be95514376109e17782c671b8
evan-poster <evan.poster@gmail.com>
2024-04-18 12:45:36 -0400

    mode_words.py added

---
+++ Miscellaneous/mode_words.py
@@ -0,0 +1,24 @@
+
+file = "thousand_words.txt"
+
+
+def main():
+    '''
+        Given a string, identify the words with the highest frequency
+    '''
+    # Import file for reading
+    with open(file, "r") as f:
+        contents = f.read().split()
+        # Count each word when it appears
+        counts = {}
+        for word in contents:
+            counts[word] = counts.get(word, 0) + 1
+            max_count = max(counts.values())
+        # We have our counts and max_count; now we just need to pull all words
+        modes = [word for word, count in counts.items() if count == max_count]
+        for word in modes:
+            print(word, counts[word])
+
+
+if __name__ == "__main__":
+    main()
